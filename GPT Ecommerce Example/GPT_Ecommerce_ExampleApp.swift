9fdc34ca031b5965a5df7eda6941350fbd176158
Marcus Kim <marcuskim1989@gmail.com>
2023-05-21 15:15:50 -0700

    Initial Commit

---
+++ GPT Ecommerce Example/GPT_Ecommerce_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPT_Ecommerce_ExampleApp.swift
+//  GPT Ecommerce Example
+//
+//  Created by Marcus Y. Kim on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct GPT_Ecommerce_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
